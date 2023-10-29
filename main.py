5d16a2cc8e203b46b5f45e97fb933fb11c163c90
Otávio Rodrigues Bambans <otavio21072002@gmail.com>
2023-10-29 11:44:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from cgfkg.cgfkg_module import CGFKG
 
 if __name__ == "__main__":
-    CGFKG('rdfs/digitalLocker_library.rdf')
-    CGFKG('rdfs/digitalLockerRef.rdf')
+    # CGFKG('rdfs/digitalLocker_library.rdf')
+    # CGFKG('rdfs/digitalLockerRef.rdf')
